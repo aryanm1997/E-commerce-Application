91b927abf602e58da2aabc5aec890ba70b66d95e
Arya N M <49433430+aryanm1997@users.noreply.github.com>
2023-02-09 23:17:29 +0530

    Add files via upload

---
+++ database/migrations/2023_02_08_161429_tbl_order.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_order', function (Blueprint $table) {
+            $table->id();
+            $table->integer('order_id')->nullable();
+            $table->string('customer_name')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('net_amount')->nullable();
+            $table->string('product_name')->nullable();
+            $table->date('order_date')->nullable();
+            $table->softDeletes();           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
